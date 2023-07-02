2dbffdab1c4929420b5956c08f5beccb25d31369
Ubuntu <howardzchung@gmail.com>
2023-07-02 09:07:36 +0000

    sameline checks

--- src/App.tsx
+++ src/App.tsx
@@ -295,18 +295,16 @@ function App() {
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <div style={{ color: 'rgb(26, 106, 255)' }} className="mobileStack mobileReverse">
               <span style={{ textTransform: 'uppercase', fontSize: 48, marginRight: 8, fontWeight: 700 }}>{electionData.overall?.['Robyn']?.toFixed(0) ?? 0}</span>
-              <span style={{ textTransform: 'uppercase', fontSize: 24, fontWeight: 500, marginRight: 8 }}>Robyn</span>
-              {isElectionOver() && (percentRobyn > percentOrlaf) && <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle" viewBox="0 0 16 16">
+              <span style={{ textTransform: 'uppercase', fontSize: 24, fontWeight: 500, marginRight: 8 }}>Robyn {isElectionOver() && (percentRobyn > percentOrlaf) && <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                 <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-              </svg>}
+              </svg>}</span>
             </div>
             <div style={{ color: 'rgb(255, 74, 67)', textAlign: 'right' }} className="mobileStack">
-              {isElectionOver() && (percentOrlaf > percentRobyn) && <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle" viewBox="0 0 16 16">
+              <span style={{ textTransform: 'uppercase', fontSize: 24, marginLeft: 8, marginRight: 8, fontWeight: 500 }}>{isElectionOver() && (percentOrlaf > percentRobyn) && <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                 <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-              </svg>}
-              <span style={{ textTransform: 'uppercase', fontSize: 24, marginLeft: 8, marginRight: 8, fontWeight: 500 }}>Orlaf</span>
+              </svg>} Orlaf</span>
               <span style={{ textTransform: 'uppercase', fontSize: 48, fontWeight: 700 }}>{electionData.overall?.['Orlaf']?.toFixed(0) ?? 0}</span>
             </div>
           </div>
