028fca2e8260ffbad1c3796d2cd1c6a01b7d4043
Howard Chung <howardzchung@gmail.com>
2025-12-24 18:27:01 -0800

    Update cache.js

--- cache.js
+++ cache.js
@@ -10,7 +10,7 @@ app.get("/", async (req, res) => {
   if (memoryCache.has('/')) {
     res.status(200).send(memoryCache.get('/'));
   } else {
-    const resp = await axios.get('https://sheets.googleapis.com/v4/spreadsheets/1Ctj7ntWMhiDUiGTaXKXJG7C7sbYnA-IjDhyvf8NCPxE/values/Form%20Responses%201?key=AIzaSyDAHivgQUlxM9FKaTYuzfKpOKgf0f9hpXI');
+    const resp = await axios.get('https://googlesheet.howardzchung.workers.dev?url=https://https://sheets.googleapis.com/v4/spreadsheets/1Ctj7ntWMhiDUiGTaXKXJG7C7sbYnA-IjDhyvf8NCPxE/values/Form%20Responses%201');
     memoryCache.set('/', resp.data, 10);
     res.status(200).send(resp.data);
   }
